98d06490a28281b6b7875026de76832942a21f0f
hugo <hugo.mayo@barclays.com>
2020-02-08 20:54:44 +0000

    Adds canvas

--- js/app.js
+++ js/app.js
@@ -4,6 +4,20 @@ import 'codemirror/theme/monokai.css';
 
 import styles from "codemirror/lib/codemirror.css";
 
+function draw(canvas) {
+  console.log("hello")
+  console.log(canvas)
+  if (canvas.getContext) {
+  var ctx = canvas.getContext('2d');
+
+  ctx.fillStyle = 'rgb(200, 0, 0)';
+  ctx.fillRect(10, 10, 50, 50);
+
+  ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+  ctx.fillRect(30, 30, 50, 50);
+  }
+}
+
 const myTextarea = document.getElementById("textarea");
 
 //console.log("here!!!");
@@ -24,3 +38,9 @@ document.getElementById("run").addEventListener("click", () => {
    console.log(typeof editor.getValue());
    eval(Sk.importMainWithBody("<stdin>", false, editor.getValue()));
 });
+
+const canvas = document.getElementById('canvas');
+console.log(document.getElementById('canvas'))
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'green';
+ctx.fillRect(10, 10, 150, 100);
